1536976ef780a93630ffd3b2bab5ee6dac46fc2e
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-03-15 12:01:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int v1 = scan.nextInt();
+    int v2 = scan.nextInt();
+
+    System.out.print("Wynik: ");
+    System.out.println(v1 + v2);
+    
+  }
+}
